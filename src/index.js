1670f1de326ea7966a93cde841a53ea51c9440de
Derek Miller <derekmiller9@gmail.com>
2016-10-03 09:08:57 -0700

    User can view a list of beers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
-fetch('/api/beers')
-.then(function(response) {
-  return response.json().then(function(json){
-    json.data.forEach(function(beer) {
-      console.log(beer.name);
-    })
-  })
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+
+const BEERS_LOADED = 'BEERS_LOADED'
+
+const beers = (_beers = [], {type, payload}) => {
+  switch (type) {
+    case BEERS_LOADED: return payload
+    default: return _beers
+  }
+}
+
+const rootReducer = combineReducers({
+  beers: beers
 })
-.catch(function(error) {
-  console.log('error');
-});
+
+const store = createStore(rootReducer);
+
+fetch('/api/beers')
+  .then(function(response) {
+    return response.json()
+  })
+  .then((json) => {
+    return json.data
+  })
+  .then((beers) => {
+    store.dispatch({ type: BEERS_LOADED, payload: beers } )
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+function renderApp() {
+  render (
+    <div>
+      { store.getState().beers.map((beer) => {
+        return <p key={beer.id}>{ beer.name }</p>
+        })
+      }
+    </div>
+    ,
+    document.getElementById('card')
+  )
+}
+
+store.subscribe(renderApp);
